dd1da2266334383304c0d279e5916c999e6738fb
omaroz46 <omar.ozan@bbzbl-it.ch>
2023-11-24 08:36:50 +0100

    Erstelle services und frontend verbessert

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { LoadingController } from "@ionic/angular";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import { environment } from "src/environments/environment";
+import { User } from "../data/user";
+
+export const USER_TABLE = 'user'
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UserService {
+
+  private supabase: SupabaseClient
+
+  constructor (private loadingCtrl: LoadingController) {
+    this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey)
+  }
+
+  createLoader() {
+    return this.loadingCtrl.create()
+  }
+
+  async getUser (id: number) {
+    const { data, error } = await this.supabase
+      .from(USER_TABLE)
+      .select('*')
+      .eq('id', id)
+      .single()
+
+    return data || {}
+  }
+
+  async updateUser (user: User) {
+    const {data, error} = await this.supabase
+      .from(USER_TABLE)
+      .update(user)
+      .eq('id', user.id)
+      .select()
+
+    return data
+  }
+
+  async createUser (user: User) {
+    const {data, error} = await this.supabase
+      .from(USER_TABLE)
+      .insert(user)
+      .select('*')
+      .single()
+
+    return data
+  }
+
+  async deleteUser (user: User) {
+    const {data, error} = await this.supabase
+      .from(USER_TABLE)
+      .delete()
+      .eq('id', user.id)
+      .select()
+
+    return data
+  }
+}
